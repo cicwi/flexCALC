e78398d17dd34b1ae0b307b7676c71959156eef0
Alexander Kostenko <a.kostenko@cwi.nl>
2018-11-21 17:20:03 +0100

    Fixed find rotation example

--- examples/example_find_rotation_axis.py
+++ examples/example_find_rotation_axis.py
@@ -16,16 +16,16 @@ path = '/ufs/ciacc/flexbox/al_test/90KV_no_filt/'
 
 proj, meta = process.process_flex(path)
 
-#%% Use optimize_rotation_center:
-    
-guess = process.optimize_rotation_center(proj, meta['geometry'], subscale = 8)
-
 #%% Reconstruct uncorrected:
 
 vol = project.init_volume(proj)
 project.FDK(proj, vol, meta['geometry'])
 
 display.display_slice(vol, bounds = [], title = 'FDK Corrected')
+
+#%% Use optimize_rotation_center:
+    
+guess = process.optimize_rotation_center(proj, meta['geometry'], subscale = 8)
 
 #%% REconstruct corrected:
 
