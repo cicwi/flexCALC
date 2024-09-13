34faf96114c73968f4fd1c0b79106414c31edc21
Alexander Skorikov <alexander.skorikov@gmail.com>
2024-09-13 11:14:34 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,85 +1,45 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""The setup script."""
-
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
-
-requirements = [
-    "numpy",
-    "tqdm",
-    "simpleitk",
-    "scipy",
-    "scikit-image",
-    "flexdata",
-    "flextomo"]
-
-setup_requirements = [ ]
-
-test_requirements = [ ]
 
 draw_requirements = [
     "networkx",
-    "pygraphviz",
-    ]
+    "pygraphviz"
+]
 
 mesh_requirements = [
-    "numpy-stl",
-    ]
+    "numpy-stl"
+]
 
 dev_requirements = [
-    'autopep8',
-    'rope',
-    'jedi',
-    'flake8',
-    'importmagic',
-    'autopep8',
-    'black',
-    'yapf',
-    'snakeviz',
-    # Documentation
-    'sphinx',
-    'sphinx_rtd_theme',
-    'myst-parser',
-    # Other
-    'bumpversion',
-    'watchdog',
-    'coverage',
-
-    ]
+    "sphinx",
+    "sphinx_rtd_theme",
+    "myst-parser"
+]
 
 setup(
     author="Alex Kostenko",
-    author_email='a.kostenko@cwi.nl',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+    description="CT data pre- and post-processing tools, simulation of spectral data, and batch-processing of large number of datasets",
+    install_requires=[
+        "numpy",
+        "tqdm",
+        "simpleitk",
+        "scipy",
+        "scikit-image",
+        "flexdata",
+        "flextomo"
     ],
-    description="ASTRA-based cone beam reconstruction routines",
-    install_requires=requirements,
     license="GNU General Public License v3",
     long_description=readme,
-    include_package_data=True,
-    keywords='flexcalc',
-    name='flexcalc',
-    packages=find_packages(include=['flexcalc']),
-    setup_requires=setup_requirements,
-    test_suite='tests',
-    tests_require=test_requirements,
-    extras_require={ 'dev': dev_requirements, "draw": draw_requirements, "mesh": mesh_requirements },
-    url='https://github.com/cicwi/flexcalc',
-    version='0.1.0',
-    zip_safe=False,
+    long_description_content_type="text/markdown",
+    name="flexcalc",
+    packages=find_packages(include=["flexcalc"]),
+    extras_require={
+        "dev": dev_requirements,
+        "draw": draw_requirements,
+        "mesh": mesh_requirements
+    },
+    url="https://github.com/cicwi/flexcalc",
+    version="0.1.0",
 )
