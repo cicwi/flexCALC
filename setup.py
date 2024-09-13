f69acbdbdd164efe34f328dd4254d368b514ab24
Alexander Skorikov <alexander.skorikov@gmail.com>
2024-09-13 11:34:43 +0200

    Update setup.py to follow the style of other flexbox packages

--- setup.py
+++ setup.py
@@ -1,45 +1,45 @@
 from setuptools import setup, find_packages
 
-with open("README.md") as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 draw_requirements = [
-    "networkx",
-    "pygraphviz"
+    'networkx',
+    'pygraphviz'
 ]
 
 mesh_requirements = [
-    "numpy-stl"
+    'numpy-stl'
 ]
 
 dev_requirements = [
-    "sphinx",
-    "sphinx_rtd_theme",
-    "myst-parser"
+    'sphinx',
+    'sphinx_rtd_theme',
+    'myst-parser'
 ]
 
 setup(
-    author="Alex Kostenko",
-    description="CT data pre- and post-processing tools, simulation of spectral data, and batch-processing of large number of datasets",
-    install_requires=[
-        "numpy",
-        "tqdm",
-        "simpleitk",
-        "scipy",
-        "scikit-image",
-        "flexdata",
-        "flextomo"
-    ],
-    license="GNU General Public License v3",
+    name='flexcalc',
+    version='1.0.0',
+    description='CT data pre- and post-processing tools, simulation of spectral data, and batch-processing of large number of datasets',
+    url='https://github.com/cicwi/flexcalc',
     long_description=readme,
-    long_description_content_type="text/markdown",
-    name="flexcalc",
-    packages=find_packages(include=["flexcalc"]),
+    long_description_content_type='text/markdown',
+    author='Alex Kostenko',
+    license='GNU General Public License v3',
+    packages=find_packages(include=['flexcalc']),
+    install_requires=[
+        'numpy',
+        'tqdm',
+        'simpleitk',
+        'scipy',
+        'scikit-image',
+        'flexdata',
+        'flextomo'
+    ],
     extras_require={
-        "dev": dev_requirements,
-        "draw": draw_requirements,
-        "mesh": mesh_requirements
-    },
-    url="https://github.com/cicwi/flexcalc",
-    version="1.0.0",
+        'dev': dev_requirements,
+        'draw': draw_requirements,
+        'mesh': mesh_requirements
+    }
 )
